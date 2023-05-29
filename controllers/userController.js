2c9d6f2eb7273d6bf413fdcb6f2f4385177c72eb
Richard Omoyele <109069693+Richiee-D@users.noreply.github.com>
2023-05-29 17:23:50 +0100

    register completed

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,7 +18,17 @@ const registerUser = asyncHandler(async (req, res) => {
   }
   //Hash Password
   const hashedPassword = await bcrypt.hash(password, 10);
-  console.log("hashed password", hashedPassword);
+  const user = await User.create({
+    username,
+    email,
+    password: hashedPassword,
+  });
+  if (user) {
+    res.status(201).json({ _id: user.id, email: user.email });
+  } else {
+    res.status(400);
+    throw new Error("User data is not valid");
+  }
   res.json({ message: "Register the user" });
 });
 
