894689355bc0f8f4f2afcd6ddf94dc3c4153cb67
Richard Omoyele <109069693+Richiee-D@users.noreply.github.com>
2023-05-29 17:15:33 +0100

    hash password added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,24 @@
 const asyncHandler = require("express-async-handler"); //works in place of try{}catch()
-const Contact = require("../models/contactModel");
+const bcrypt = require("bcrypt"); // hashing password
+const User = require("../models/userModel");
 
 //@desc Register a user
 //@route POST /api/users/register
 //@access public
 const registerUser = asyncHandler(async (req, res) => {
+  const { username, password, email } = req.body;
+  if (!username || !password || !email) {
+    res.status(400);
+    throw new Error("All fields are mandatory");
+  }
+  const userAvailable = await User.findOne({ email });
+  if (userAvailable) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+  //Hash Password
+  const hashedPassword = await bcrypt.hash(password, 10);
+  console.log("hashed password", hashedPassword);
   res.json({ message: "Register the user" });
 });
 
