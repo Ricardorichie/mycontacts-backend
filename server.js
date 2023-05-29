4f0fa18b27df88e61ffa0eff5e9bcbc07f43e81a
Richard Omoyele <109069693+Richiee-D@users.noreply.github.com>
2023-05-29 01:45:26 +0100

    Middleware for Error handling

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const errorHandler = require("./middleware/errorHandler");
 
 const dotenv = require("dotenv").config();
 
@@ -8,6 +9,8 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json()); //body-parser for data parsed in req
 app.use("/api/contacts", require("./routes/contactRoutes"));
+
+app.use(errorHandler); //handle error
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
